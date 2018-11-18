3c0f45051b336db0314680cd79661302e2ebe076
Tyler Reisinger <reisinger.tyler@gmail.com>
2018-11-18 11:05:52 -0500

    Added a test for multiple argument functions

---
+++ tests/multiple_args.rs
@@ -0,0 +1,20 @@
+#[test]
+fn multiple_args() {
+    use lru_cache_macros::lru_cache;
+
+    #[lru_cache(200)]
+    fn ackermann(m: u64, n: u64) -> u64 {
+        if m == 0 {
+            n + 1
+        } else if m > 0 && n == 0 {
+            ackermann(m - 1, 1)
+        } else {
+            ackermann(m - 1, ackermann(m, n - 1))
+        }
+    }
+
+    assert_eq!(ackermann(0, 0), 1);
+    assert_eq!(ackermann(1, 0), 2);
+    assert_eq!(ackermann(1, 1), 3);
+    assert_eq!(ackermann(3, 2), 29);
+}
