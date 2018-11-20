01d061b4a2cff7c6f7a9a1324f7445bf63506d1b
Tyler Reisinger <reisinger.tyler@gmail.com>
2018-11-20 10:26:42 -0500

    Implemented the ability to use either TLS or a Mutex for the cache

--- tests/config.rs
+++ tests/config.rs
@@ -2,12 +2,30 @@ use lru_cache_macros::lru_cache;
 use std::thread;
 use std::sync::atomic::{AtomicUsize, Ordering};
 
-static CALL_COUNT: AtomicUsize = AtomicUsize::new(0);
-
 #[test]
-fn ignore_args() {
+fn thread_local_ignore_args() {
     #[lru_cache(20)]
     #[lru_config(ignore_args = call_count)]
+    #[lru_config(thread_local)]
+    fn fib(x: u32, call_count: &mut u32) -> u64 {
+        *call_count += 1;
+        if x <= 1 {
+            1
+        } else {
+            fib(x - 1, call_count) + fib(x - 2, call_count)
+        }
+    }
+
+    let mut call_count = 0;
+    assert_eq!(fib(39, &mut call_count), 102_334_155);
+    assert_eq!(call_count, 40);
+}
+
+#[test]
+fn multithreaded() {
+    static CALL_COUNT: AtomicUsize = AtomicUsize::new(0);
+
+    #[lru_cache(20)]
     fn fib(x: u32) -> u64 {
         CALL_COUNT.fetch_add(1, Ordering::SeqCst);
         if x <= 1 {
